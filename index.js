fb253520913f91c9c2bc190060a8c8585da256e2
Dima <d.klychnikov@petrosoft.su>
2023-03-28 12:04:03 +0300

    dynamic articles with jjs

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const model = [
         value: [
             '111111111', 
             '222222222',
-            '333333333'
+            '333333333',
+            '444444444'
         ]
     }
 ]
@@ -22,12 +23,49 @@ const $site = document.querySelector('#site')
 model.forEach(block => {
     let html = ''
     if (block.type === 'title') {
-        html = `
-            <div class="row">
-                <div class="col-sm">
-                    <h1>hello world</h1>
-                </div>
+        html = title(block)
+    } else if (block.type === 'text') {
+        html = text(block)
+    } else if (block.type === 'columns') {
+        html = columns(block)
+    }
+
+    $site.insertAdjacentHTML('beforeend', html)
+})
+
+function title(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+        </div>
+    `
+}
+
+function text(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <p>${block.value}</p>
+            </div>
+        </div>
+    ` 
+}
+
+function columns(block) {
+    let html = ''
+    block.value.forEach(item => {
+        html += `
+            <div class="col-sm">
+                <p>${item}</p>
             </div>
         `
-    }
-})
+    })
+    return `
+        <div class="row">
+            ${html}
+        </div>
+    
+    `
+}
